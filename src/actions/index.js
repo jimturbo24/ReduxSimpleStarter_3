a0e36240be6406f319f06da1bbfac506509139e4
James Trbovich <jim.trbovich@gmail.com>
2018-02-15 16:08:06 -0600

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'e7e45d431a67a77ffb8f40994fa4668f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
